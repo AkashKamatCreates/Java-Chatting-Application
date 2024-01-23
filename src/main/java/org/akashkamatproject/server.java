389c4ae20508f4bd5825302c2c907d3a3d484d38
AkashKamatCreates <kamatakash13@gmail.com>
2024-01-23 22:38:24 +0530

    added comment about resource package

--- src/main/java/org/akashkamatproject/server.java
+++ src/main/java/org/akashkamatproject/server.java
@@ -18,7 +18,7 @@ public class server extends JFrame {
         add(p1);
 
 
-
+/** dev comment: imageicon is stored in resource package strictly!!! */
         ImageIcon dp1 = new ImageIcon(ClassLoader.getSystemResource("icons/profile.png"));
         Image i1 = dp1.getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT);
         ImageIcon dp1f = new ImageIcon(i1);
